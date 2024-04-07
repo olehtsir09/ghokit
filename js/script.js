79329327cd77bae824cf0e12bc73ea8e21b0afa4
olehtsir <olehtsir09@gmail.com>
2024-04-07 21:35:57 +0200

    update js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ let x = 0;
 for(x = 0; x<=10; x++){
     console.log(x);
 }
+
+
+
+let userName = 'Oleh';
+let userAge = +prompt("How old are you?");
+
+if(userAge >= 18){
+    alert("Welcome");
+} else{
+    alert("You can't open this page");
+}
